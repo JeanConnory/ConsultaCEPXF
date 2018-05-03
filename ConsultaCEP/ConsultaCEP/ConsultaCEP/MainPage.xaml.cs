4d9a678982ab59d407a85829cf227fdd112f27a9
JeanConnory <michaelrhcp@hotmail.com>
2018-05-02 22:44:41 -0300

    Adicionando o projeto de consulta CEP

---
+++ ConsultaCEP/ConsultaCEP/ConsultaCEP/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using ConsultaCEP.Servico;
+using ConsultaCEP.Servico.Modelo;
+using System;
+using Xamarin.Forms;
+
+namespace ConsultaCEP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BOTAO.Clicked += BOTAO_Clicked;
+        }
+
+        private void BOTAO_Clicked(object sender, EventArgs e)
+        {
+            string cep = CEP.Text.Trim();
+
+            if (IsValidCEP(cep))
+            {
+                try
+                {
+                    Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+
+                    if (end != null)
+                        RESULTADO.Text = string.Format("Endereço: {0}, {1}, {2} - {3}", end.logradouro, end.bairro, end.localidade, end.uf);
+                    else
+                        DisplayAlert("ERRO", "O endereço não foi encontrado para o CEP Informado: " + cep, "OK");
+                }
+                catch(Exception ex)
+                {
+                    DisplayAlert("ERRO CRÍTICO", ex.Message, "OK");
+                }
+                
+            }
+        }
+
+        private bool IsValidCEP(string cep)
+        {
+            bool valido = true;
+
+            if (cep.Length != 8)
+            {
+                DisplayAlert("ERRO", "O CEP deve conter 8 caaracteres.", "OK");
+                valido = false;
+            }
+            int novoCEP = 0;
+            if (!int.TryParse(cep, out novoCEP))
+            {
+                DisplayAlert("ERRO", "O CEP deve ser composto apenas por números.", "OK");
+                valido = false;
+            }
+            return valido;
+        }
+    }
+}
---
+++ ConsultaCEP/ConsultaCEP/ConsultaCEP/Servico/ViaCEPServico.cs
@@ -0,0 +1,25 @@
+﻿using ConsultaCEP.Servico.Modelo;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace ConsultaCEP.Servico
+{
+    public class ViaCEPServico
+    {
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string novoEnderecoURL = string.Format(EnderecoURL, cep);
+            WebClient wc = new WebClient();
+            string conteudo = wc.DownloadString(novoEnderecoURL);
+
+            Endereco oEndereco = JsonConvert.DeserializeObject<Endereco>(conteudo);
+
+            if (oEndereco.cep == null)
+                return null;
+
+            return oEndereco;
+        }
+    }
+}
